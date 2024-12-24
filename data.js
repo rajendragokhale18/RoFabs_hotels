6c5a324f8585e81f041008589c9c5515ba433139
RAJENDRA <118453755+rajendragokhale18@users.noreply.github.com>
2024-12-24 20:11:48 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,43 @@
+{
+    "data": {
+        "attributes": {
+            "id": "7d635d1f-7f2a-48e0-a70a-c2cd55f67c5a",
+            "title": "sdvbdaadv",
+            "currency": "AFN",
+            "cancellation_policy_deadline": null,
+            "cancellation_policy_deadline_type": null,
+            "after_reservation_cancellation_logic": "free",
+            "guarantee_payment_policy": "none",
+            "guarantee_payment_amount": null,
+            "non_show_policy": "total_price",
+            "cancellation_policy_penalty": null,
+            "cancellation_policy_mode": null,
+            "cancellation_policy_logic": "free",
+            "after_reservation_cancellation_amount": null,
+            "associated_rate_plan_ids": []
+        },
+        "id": "7d635d1f-7f2a-48e0-a70a-c2cd55f67c5a",
+        "type": "cancellation_policy"
+    }
+}
+
+
+
+// Payload
+
+[
+    {
+        "property_id":"1392d04e-eec2-4b67-9536-d8f70b6fda98",
+        "title":"sdvbdaadv",
+        "currency":"AFN"
+        "cancellation_policy_deadline_type":"days",
+        "after_reservation_cancellation_logic":"free",
+        "guarantee_payment_policy":"none",
+        "guarantee_payment_amount":null,
+        "cancellation_policy_logic":"free",
+        "cancellation_policy_mode":"percent",
+        "non_show_policy":"total_price",
+        "associated_rate_plan_ids":[],
+    }
+]
+
